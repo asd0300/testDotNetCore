483aa0db51364aa7cd3fa76b4c3990f087fdd374
Sol_Ben_Fan <ben.fan@solventosoft.com.tw>
2023-02-18 16:45:17 +0800

    Get 給multi value , 參數過濾

--- tododotnet6prac/tododotnet6prac/Controllers/TodoController.cs
+++ tododotnet6prac/tododotnet6prac/Controllers/TodoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using tododotnet6prac.Dtos;
 using tododotnet6prac.Models;
+using tododotnet6prac.Parameters;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,7 +22,46 @@ namespace tododotnet6prac.Controllers
         }
         // GET: api/<TodoController>
         [HttpGet]
-        public IEnumerable<TodoListSelectDto> Get()
+        //public IEnumerable<TodoListSelectDto> Get()
+        //{
+        //    var result = from a in _todoContext.TodoLists
+        //                 where a.Name == "Item2"
+        //                 select a;
+        //    var result3 = _todoContext.TodoLists.ToList();
+        //    var result2 = _todoContext.TodoLists.Where(a => a.Name == "walk dog");
+
+
+        //    //try dto
+        //    var result4 = _todoContext.TodoLists.Select(a => new TodoListSelectDto
+        //    {
+        //        TodoId = a.TodoId,
+        //        Name = a.Name,
+        //        InsertTime= a.InsertTime,
+        //        UpdateTime = a.UpdateTime,
+        //        Enable = a.Enable,
+        //        Orders= a.Orders,
+        //        InsertEmployee= a.Name,
+        //        UpdateEmployee= a.Name,
+        //    });
+        //    // try automapper
+        //    var result5 = _todoContext.TodoLists;
+
+        //    return _iMapper.Map<IEnumerable<TodoListSelectDto>>(result5);
+        //}
+
+        [HttpGet]
+        public IEnumerable<TodoListSelectDto> Get([FromQuery]TodoSelectParameters value) //bool 可不輸入 //修改成TodoSelectParameter時要用于指定参数如何绑定到 ASP.NET (有時asp,net 預設可能是錯誤的)
+            //[FromQuery]：表示該參數的值應該從請求的查詢字符串中獲取。這通常用於獲取 GET 請求的參數。
+
+            //[FromBody]：表示該參數的值應該從請求的正文中獲取。這通常用於獲取 POST、PUT 或 DELETE 請求的數據。在這種情況下，請求主體必須是可反序列化為參數類型的格式，例如 JSON 或 XML。
+
+            //[FromHeader]：表示該參數的值應該從請求頭中獲取。例如，[FromHeader(Name = "Authorization")] 表示該參數的值應該從名為 "Authorization" 的請求頭中獲取。
+
+            //[FromForm]：表示該參數的值應該從請求正文中的表單數據中獲取。這通常用於從 HTML 表單中獲取數據。
+
+            //[FromRoute]：表示該參數的值應該從請求 URL 中的路由參數中獲取。例如，[FromRoute(Name = "id")] 表示該參數的值應該從名為 "id" 的路由參數中獲取。
+
+            //[FromServices]：表示該參數的值應該從依賴注入服務中獲取。這通常用於從控制器外部獲取服務的實例。
         {
             var result = from a in _todoContext.TodoLists
                          where a.Name == "Item2"
@@ -35,17 +75,39 @@ namespace tododotnet6prac.Controllers
             {
                 TodoId = a.TodoId,
                 Name = a.Name,
-                InsertTime= a.InsertTime,
+                InsertTime = a.InsertTime,
                 UpdateTime = a.UpdateTime,
                 Enable = a.Enable,
-                Orders= a.Orders,
-                InsertEmployee= a.Name,
-                UpdateEmployee= a.Name,
+                Orders = a.Orders,
+                InsertEmployee = a.Name,
+                UpdateEmployee = a.Name,
             });
             // try automapper
             var result5 = _todoContext.TodoLists;
 
-            return _iMapper.Map<IEnumerable<TodoListSelectDto>>(result5);
+            if(!string.IsNullOrWhiteSpace(value.name))
+            {
+                result4 = result4.Where(a => a.Name.Contains(value.name));
+            }
+
+            if(value.enable != null) 
+            {
+                result4 = result4.Where(a=>a.Enable == value.enable);
+            }
+
+            if(value.UpdateTime != null)
+            {
+                result4 = result4.Where(a=>a.UpdateTime.Date ==value.UpdateTime);
+            }
+
+            if (value.minOrder != null && value.maxOrder != null)
+            {
+                result4 = result4.Where(a => a.Orders>value.minOrder && a.Orders < value.maxOrder);
+            }
+
+
+
+            return result4;
         }
 
         // GET api/<TodoController>/5
---
+++ tododotnet6prac/tododotnet6prac/Parameters/TodoSelectParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace tododotnet6prac.Parameters
+{
+    public class TodoSelectParameters
+    {
+        public string? name { get; set; }
+        public bool? enable { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public int? minOrder { get; set; }
+        public int? maxOrder { get; set; }
+
+
+    }
+}
